227a0b7b29e0b85fe63c9e3e83f4e63ed0017c14
Chin Leung <hello@chinleung.com>
2021-05-11 15:35:01 -0400

    Remove namespace

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ShowDemoPageController;
+use App\Http\Controllers\ShowUserController;
 use Illuminate\Support\Facades\Route;
 
-Route::multilingual('/', 'ShowDemoPageController')->name('home');
-Route::multilingual('test', 'ShowDemoPageController');
-Route::multilingual('show-user', 'ShowUserController')->name('user.show');
+Route::multilingual('/', ShowDemoPageController::class)->name('home');
+Route::multilingual('test', ShowDemoPageController::class);
+Route::multilingual('show-user', ShowUserController::class)->name('user.show');
